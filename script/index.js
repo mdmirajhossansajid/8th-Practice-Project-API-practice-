39fe0eaab19f744a43eb6f433fcab7158d3b6ac7
mdmirajhossansajid <mdmirajhossansajidpc@gmail.com>
2026-04-25 23:58:34 +0600

    added new feature

--- script/index.js
+++ script/index.js
@@ -1,22 +1,71 @@
 const catagoriesContainer=document.getElementById("catagoriesContainer");
 const treesContainer=document.getElementById("treesContainer");
+const loadSpinner=document.getElementById("loadSpinner");
+
 async function loadCatagories(){
     const res=await fetch("https://openapi.programming-hero.com/api/categories");
     const data=await res.json();
     data.categories.forEach((category)=>{
         const btn=document.createElement("button");
         btn.className="btn btn-outline w-full";
-        btn.innerHTML=category.category_name;
+        btn.textContent=category.category_name;
+        btn.onclick=()=>selectCatagories(category.id, btn); 
         catagoriesContainer.appendChild(btn);
     });
+    
 }
+async function selectCatagories(categoryId, button){
+        showLoading();
+        const allbuttons=document.querySelectorAll("#catagoriesContainer button, #allTreesbtn");
+        allbuttons.forEach((btn)=>{
+            btn.classList.remove("btn-success");
+            btn.classList.add("btn-outline");
+        });
+        button.classList.add("btn-success");
+        button.classList.remove("btn-outline");
+        const res = await fetch(`https://openapi.programming-hero.com/api/category/${categoryId}`);
+      const data = await res.json();
+
+    displayTrees(data.plants);
+    hideLoading();
+    }
+
+function showLoading() {
+  loadSpinner.classList.remove("hidden");
+  loadSpinner.classList.add("flex"); 
+  treesContainer.innerHTML = "";
+}
+function hideLoading() {
+  loadSpinner.classList.add("hidden");
+  loadSpinner.classList.remove("flex");
+}
+allTreesbtn.addEventListener("click", () => {
+  // Update active button style
+  const allButtons = document.querySelectorAll(
+    "#catagoriesContainer button, #allTreesbtn",
+  );
+  //   console.log(allButtons);
+  allButtons.forEach((btn) => {
+    btn.classList.remove("btn-success");
+    btn.classList.add("btn-outline");
+  });
+
+  allTreesbtn.classList.add("btn-success");
+  allTreesbtn.classList.remove("btn-outline");
+
+  loadTrees();
+});
 async function loadTrees(){
+    showLoading();
     const res=await fetch("https://openapi.programming-hero.com/api/plants");
     const data=await res.json();
     displayTrees(data.plants);
+    console.log(data.plants);
+    hideLoading();
 
 }
 function displayTrees(trees){
+    treesContainer.innerHTML = "";
     trees.forEach((tree)=>{
         const card=document.createElement("div");
         card.className="card bg-white shadow-sm";
